da005019fe96203c30998f37a3cdfe5af792d658
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-12-20 00:48:08 +0600

    07:  ai providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,15 @@
 import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+
 export const appRouter = createTRPCRouter({
+  testAI: protectedProcedure.mutation(async () => {
+    await inngest.send({ name: "execute/ai" });
+
+    return {success: true, message: "Job queued"};
+  }),
   getUsers: protectedProcedure.query(({ ctx }) => {
     return prisma.user.findUniqueOrThrow({
       where: {
